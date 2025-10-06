89bcd741527aa354caa86c432360b6998cb34258
Developer9719 <justin.bat19@gmail.com>
2025-10-05 22:58:50 -0400

    Started factory functions for taskLists and projectLists

--- src/script/projectLogic.js
+++ src/script/projectLogic.js
@@ -1,1 +1,15 @@
 // Need a default project that auto opens when the app is run
+
+export const createProject = () => {
+    return {
+        projectTitle,
+        projectDescription,
+        projectDueDate,
+        projectPriority,
+        projectToDos: [],
+        isDefault: false,
+        addToDo(/* Pass in object to add to projectToDos array */) {
+            
+        }
+    }
+}
--- src/script/taskList.js
+++ src/script/taskList.js
@@ -1,2 +1,12 @@
 // These need to be objects
 // Need to be dynamically created with factory functions or classes
+
+export const taskList = () => {
+    taskTitle,
+    taskDescription,
+    taskPriority,
+    isComplete: false,
+    editTask(/* Pass in object to edit task details */) {
+        
+    }
+}
